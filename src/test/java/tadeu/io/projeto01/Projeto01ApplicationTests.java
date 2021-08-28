1645fcba1853c0db4416ffbabe4d597a6e0ddc83
Tadeu Costa <tadeu@MacBook-Air-de-Antonio.local>
2021-08-28 18:21:18 -0300

    primeiro commit

---
+++ src/test/java/tadeu/io/projeto01/Projeto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package tadeu.io.projeto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
